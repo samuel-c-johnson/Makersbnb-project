2198797629a721faa1878db8a133de45ee20c2f4
Samuel Johnson <samjohnson@Samuels-MacBook-Pro.local>
2018-01-04 15:58:46 +0000

    Required bodyParser and managed to store lease parameters

--- server.js
+++ server.js
@@ -1,43 +1,67 @@
 // server.js
 // load the things we need
 var express = require('express');
-var app = express();
 var session = require('express-session');
+var app = express();
+var bodyParser = require('body-parser');
+
 
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // var sess = {
-// app.use(session(sess);
+app.use(session({
+ secret: 'property',
+ saveUninitialized: false,
+ resave: false
+}));
 
 // use res.render to load up an ejs view file
 
 // index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+ res.render('pages/index');
 });
 
 app.get('/lease', function(req, res) {
-  res.render('pages/lease');
+ res.render('pages/lease');
 });
 
 app.get('/rent', function(req, res) {
-  res.render('pages/rent');
+ res.render('pages/rent');
 });
 
 app.post('/lease/new', function(req, res) {
-  // sess = req.session.name;
+  // console.log(req.session)
+  // req.session.userName = req.query.name;
+  // res.send(req.session)
+  params = req.body
+
   res.redirect('pages/listedProperty');
 });
 
 app.get('/lease/pages/listedProperty', function(req, res) {
-  res.render('pages/listedProperty');
+  console.log(params)
+ res.render('pages/listedProperty', {params});
 });
 
 app.listen(8080);
 console.log('8080 is the magic port')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
